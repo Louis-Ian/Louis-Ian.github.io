b38b9877a72ad434c5f1a849a51f6f8fdbdf39ad
Louis Ian <louisiangt@gmail.com>
2022-08-28 09:34:41 -0300

    Defined colors and other constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color persimmonOrange = Color(0xFFFD650D);
+const Color turquoiseBlue = Color(0xFF4CE5C4);
+const Color lavenderBlue = Color(0xFFB8C6F2);
+const Color oxfordBlue = Color(0xFF09172C);
+const Color onyxGrey = Color(0xFF373B43);
+
+const double defaultPadding = 20.0;
+const Duration defaultDuration = Duration(seconds: 1);
+const double maxWidth = 1440.0;
